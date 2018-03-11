4e83b53aa4aec171322ca7c619a0eb7812d7cca9
cjuhnke <chris.juhnke@kellogg.northwestern.edu>
2018-03-11 12:55:57 -0500

    Add rankings page

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { ListView, StyleSheet, Image, View} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 import { Body, Title, Right, Container, Header, Content, Icon, List, ListItem, Text, Card, CardItem, Left, Button} from 'native-base';
 
-export default class App extends React.Component {
+
+class HomeScreen extends React.Component {
   constructor() {
     super();
     this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
@@ -65,7 +67,7 @@ export default class App extends React.Component {
     let requestOptions = {
       method: 'PATCH',
       headers: new Headers({
-        'Authorization': 'Bearer keykybUZUy6yWeJHv', // replace with your own API key
+        'Authorization': 'Bearer keykybUZUy6yWeJHv',
         'Content-type': 'application/json'
       }),
       body: JSON.stringify({
@@ -151,16 +153,133 @@ export default class App extends React.Component {
                 <Icon name="arrow-forward" />
             </Button>}
         </View>
+        <View style={styles.ranker}>
+          {this.state.players[0] &&
+            <Button stylesiconLeft onPress={() => this.props.navigation.navigate('Details')}>
+              <Text>Go to Details</Text>
+            </Button>}
+        </View>
       </Container>
     );
   }
 }
 
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
+
+class DetailsScreen extends React.Component {
+  constructor() {
+    super();
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      playerList: []
+    }
+  }
+
+  // Retrieve the list of ideas from Airtable
+  getList() {
+    // Airtable API endpoint, replace with your own
+    let airtableUrl = "https://api.airtable.com/v0/appuHktN2ZLgfKPwG/players?maxRecords=500&view=Grid%20view";
+
+    // Needed for Airtable authorization, replace with your own API key
+    let requestOptions = {
+      headers: new Headers({
+        'Authorization': 'Bearer keykybUZUy6yWeJHv'
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.setState({
+        playerList: json.records
+      });
+    });
+  }
+
+  // Runs when the application loads (i.e. the "App" component "mounts")
+  componentDidMount() {
+    this.getList(); // refresh the list when we're done
+  }
+
+  // The UI for each row of data
+  renderRow(data) {
+    return (
+      <ListItem style={{ paddingLeft: 20, paddingRight: 20 }}>
+        <Body>
+          <Text>{data.fields.first + " " + data.fields.last}</Text>
+        </Body>
+        <Right>
+          <Text note>{data.fields.votes} votes</Text>
+        </Right>
+      </ListItem>
+    )
+  }
+
+  // The UI for what appears when you swipe right
+  renderSwipeRight(data, secId, rowId, rowMap) {
+  }
+
+  // The UI for what appears when you swipe left
+  renderSwipeLeft(data, secId, rowId, rowMap) {
+  }
+
+  render() {
+    let sortedPlayers = this.state.playerList.sort((a,b) => {
+      if (a.fields.votes < b.fields.votes) {
+        return 1;
+      }
+      if (a.fields.votes > b.fields.votes) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+    let rows = this.ds.cloneWithRows(sortedPlayers);
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Title>Player Rankings</Title>
+          </Body>
+        </Header>
+        <Content>
+          <List
+            dataSource={rows}
+            renderRow={(data) => this.renderRow(data)}
+            renderLeftHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeRight(data, secId, rowId, rowMap)}
+            renderRightHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeLeft(data, secId, rowId, rowMap)}
+          />
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'gray',
-    top: 50,
+    top: 0,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -173,11 +292,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flex: 1,
     position: "absolute",
-    bottom: 50,
+    bottom: 40,
     left: 0,
     right: 0,
     justifyContent: 'space-between',
     padding: 15,
+  },
+  ranker: {
+    flexDirection: "row",
+    flex: 1,
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    padding: 0,
   },
   start: {
     flexDirection: "row",
